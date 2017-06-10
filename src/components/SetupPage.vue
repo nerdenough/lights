03235c202688d58f8f03334c1f2d2e4de9c4bbee
Brendan Goodenough <brendan@goodenough.nz>
2017-06-10 17:04:32 +1200

    Add setup page

---
+++ src/components/SetupPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="setup">
+    <form class="form">
+      <h1 class="title">Lights</h1>
+      <input type="text" class="address" v-model="address" placeholder="Your Hue IP Address">
+      <button class="submit">Connect</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'setup',
+  data: () => ({
+    address: ''
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../app.global.scss';
+
+.setup {
+  width: 100%;
+  height: 100%;
+  background: $dark-grey;
+}
+
+.form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 320px;
+  min-width: 280px;
+  text-align: center;
+
+  .title {
+    color: white;
+    font-size: 48px;
+    margin-bottom: 20px;
+  }
+
+  .address {
+    width: 100%;
+    color: $medium-grey;
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+    outline: none;
+    padding: 20px;
+    @include border;
+    @include box-sizing;
+
+    &::placeholder {
+      color: $medium-grey;
+    }
+  }
+
+  .submit {
+    background: $green;
+    color: white;
+    font-size: 18px;
+    font-weight: bold;
+    cursor: pointer;
+    outline: none;
+    margin-top: 20px;
+    padding: 20px;
+    @include border;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import SetupPage from '@/components/SetupPage'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'Hello',
+    component: Hello
+  }, {
+    path: '/setup',
+    name: 'Setup',
+    component: SetupPage
+  }]
 })
