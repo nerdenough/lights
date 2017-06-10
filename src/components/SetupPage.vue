1baaab2676e07fa057a76991ef6cdc88ee76136f
Brendan Goodenough <brendan@goodenough.nz>
2017-06-10 18:03:03 +1200

    Move setup form to its own component

--- src/components/SetupPage.vue
+++ src/components/SetupPage.vue
@@ -1,22 +1,19 @@
 <template>
   <div class="setup">
-    <div class="form">
-      <h1 class="title">Lights</h1>
-      <input class="address" type="text" v-model="address" placeholder="Your Hue IP Address">
-      <p class="error" v-if="error">{{ error }}</p>
-      <button class="submit" @click="connect">Connect</button>
-    </div>
+    <setup-form :connect="connect" :error="error"></setup-form>
   </div>
 </template>
 
 <script>
-async function getUsername () {
-  const res = await this.$http.post(`http://${this.address}/api`, {
+import SetupForm from '@/components/SetupPage/SetupForm.vue'
+
+async function getUsername (address) {
+  const res = await this.$http.post(`http://${address}/api`, {
     devicetype: 'lights#web'
   }).then(res => res.body[0])
 
   if (res.error) {
-    setTimeout(this.getUsername, 1000)
+    setTimeout(this.getUsername, 1000, address)
     console.log('press link')
   } else {
     const username = res.success.username
@@ -24,10 +21,11 @@ async function getUsername () {
   }
 }
 
-async function connect () {
+async function connect (address) {
+  this.error = null
   try {
-    await this.$http.get(`http://${this.address}/api/test/config`)
-    await this.getUsername()
+    await this.$http.get(`http://${address}/api/test/config`)
+    await this.getUsername(address)
   } catch (err) {
     this.error = 'Unable to find a Hue bridge at this address!'
   }
@@ -35,8 +33,10 @@ async function connect () {
 
 export default {
   name: 'setup',
+  components: {
+    SetupForm
+  },
   data: () => ({
-    address: '',
     error: null
   }),
   methods: {
@@ -53,55 +53,5 @@ export default {
   width: 100%;
   height: 100%;
   background: $dark-grey;
-}
-
-.form {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  max-width: 320px;
-  min-width: 280px;
-  text-align: center;
-
-  .title {
-    color: white;
-    font-size: 48px;
-    margin-bottom: 20px;
-  }
-
-  .address {
-    width: 100%;
-    color: $medium-grey;
-    font-size: 18px;
-    font-weight: bold;
-    text-align: center;
-    outline: none;
-    margin-bottom: 10px;
-    padding: 20px;
-    @include border;
-    @include box-sizing;
-
-    &::placeholder {
-      color: $medium-grey;
-    }
-  }
-
-  .error {
-    color: $red;
-    font-size: 16px;
-  }
-
-  .submit {
-    background: $green;
-    color: white;
-    font-size: 18px;
-    font-weight: bold;
-    cursor: pointer;
-    outline: none;
-    margin-top: 10px;
-    padding: 20px;
-    @include border;
-  }
 }
 </style>
---
+++ src/components/SetupPage/SetupForm.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="setup-form">
+    <h1 class="title">Lights</h1>
+    <input class="address" type="text" v-model="address" placeholder="Your Hue IP Address">
+    <p class="error" v-if="error">{{ error }}</p>
+    <button class="submit" @click="connect(address)">Connect</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'setup-page',
+  props: {
+    error: String,
+    connect: {
+      type: Function,
+      required: true
+    }
+  },
+  data: () => ({
+    address: ''
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../app.global.scss';
+
+.setup-form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 320px;
+  min-width: 280px;
+  text-align: center;
+
+  .title {
+    color: white;
+    font-size: 48px;
+    margin-bottom: 20px;
+  }
+
+  .address {
+    width: 100%;
+    color: $medium-grey;
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+    outline: none;
+    margin-bottom: 10px;
+    padding: 20px;
+    @include border;
+    @include box-sizing;
+
+    &::placeholder {
+      color: $medium-grey;
+    }
+  }
+
+  .error {
+    color: $red;
+    font-size: 16px;
+  }
+
+  .submit {
+    background: $green;
+    color: white;
+    font-size: 18px;
+    font-weight: bold;
+    cursor: pointer;
+    outline: none;
+    margin-top: 10px;
+    padding: 20px;
+    @include border;
+  }
+}
+</style>
